da628bea4f699c82e7a20c769f4d1191c343daf8
Le Diem Hao <lediemhao@gmail.com>
2019-03-10 21:22:38 +0700

    Update Topic_03_WebBrowser + Topic_04_TextBox_TextArea Exercise TC01, TC02

--- src/selenium/Topic_03_WebElement_WebDriver.java
+++ src/selenium/Topic_03_WebElement_WebDriver.java
@@ -1,17 +1,21 @@
 package selenium;
 
 import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
 import org.testng.annotations.BeforeTest;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 
 public class Topic_03_WebElement_WebDriver {
-	
+
 	WebDriver driver;
 	//Email/ Age (Under 18)/ Education/ Job Role 01/ Interests (Development)/ Slider 01/ Button is enabled
 	By emailTxt = By.xpath("//input[@id='mail']");
@@ -29,75 +33,100 @@ public class Topic_03_WebElement_WebDriver {
 	By interestsDisCheckbox = By.xpath("//input[@id='check-disbaled']");
 	By slider02 = By.xpath("//input[@id='slider-2']");
 	By buttonDisable = By.xpath("//button[@id='button-disabled']");
- 
-  @BeforeTest
-  public void beforeTest() {
-	  driver = new FirefoxDriver();
-	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  driver.manage().window().maximize();
-	  driver.get("https://daominhdam.github.io/basic-form/index.html");
-  } 
-  
-  @Test
-  public void TC_01_CheckDisplayed() {
 
-	  if(isElementDisplayed(emailTxt)) {
-		  driver.findElement(emailTxt).sendKeys("Automation Testing");
-	  }
-	  if(isElementDisplayed(educationTxtArea)) {
-		  driver.findElement(educationTxtArea).sendKeys("Automation Testing");
-	  }
-	  if(isElementDisplayed(ageUnder18Radio)) {
-		  driver.findElement(ageUnder18Radio).click();
-	  }
+	@BeforeTest
+	public void beforeTest() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://daominhdam.github.io/basic-form/index.html");
+	} 
 
-  }
-  
 
-  @Test
-  public void TC_02_CheckEnable() {
-		 isElementEnabled(emailTxt);
-		 isElementEnabled(ageUnder18Radio);
-		 isElementEnabled(educationTxtArea);
-		 isElementEnabled(jobRole1DropDown);
-		 isElementEnabled(slider1);
-		 isElementEnabled(buttonEnable);
-		 isElementEnabled(passWordTxt);
-		 isElementEnabled(ageRadiobtn);
-		 isElementEnabled(biographyTxtArea);
-		 isElementEnabled(jobRole2Dropdown);
-		 isElementEnabled(interestsDisCheckbox);
-		 isElementEnabled(slider02);
-		 isElementEnabled(buttonDisable);
-  }
-  
-  @Test
-  public void TC_03_CheckIsSelected() {
-	  if(!driver.findElement(ageUnder18Radio).isSelected()) {
-		  driver.findElement(ageUnder18Radio).click();
-	  } if (!driver.findElement(interestDevCheckbox).isSelected()) {
-		driver.findElement(interestDevCheckbox).click();
+	public void TC_01_CheckDisplayed() {
+
+		if(isElementDisplayed(emailTxt)) {
+			driver.findElement(emailTxt).sendKeys("Automation Testing");
+		}
+		if(isElementDisplayed(educationTxtArea)) {
+			driver.findElement(educationTxtArea).sendKeys("Automation Testing");
+		}
+		if(isElementDisplayed(ageUnder18Radio)) {
+			driver.findElement(ageUnder18Radio).click();
+		}
+
 	}
-  }
-  
-  
 
-  
-  @AfterTest
-  public void afterTest() {
-	  driver.quit();
-  }
 
-  public boolean isElementDisplayed(By byVAlue) {
-	  return driver.findElement(byVAlue).isDisplayed();
-  }
-  
-  public void isElementEnabled(By byValue) {
-	if(driver.findElement(byValue).isEnabled()) {
-		System.out.println(driver.findElement(byValue).getTagName() + " --- Element is Enabled");
-	}else {
-		System.out.println(driver.findElement(byValue).getTagName() +" *** Element is Disabled");
+	@Test(enabled = false)
+	public void TC_02_CheckEnable() {
+		Assert.assertTrue(isElementEnabled(emailTxt));
+		Assert.assertTrue(isElementEnabled(ageUnder18Radio));
+		Assert.assertTrue(isElementEnabled(educationTxtArea));
+		Assert.assertTrue(isElementEnabled(jobRole1DropDown));
+		Assert.assertTrue(isElementEnabled(slider1));
+		Assert.assertTrue(isElementEnabled(buttonEnable));
+		Assert.assertFalse(isElementEnabled(passWordTxt));
+		Assert.assertFalse(isElementEnabled(ageRadiobtn));
+		Assert.assertFalse(isElementEnabled(biographyTxtArea));
+		Assert.assertFalse(isElementEnabled(jobRole2Dropdown));
+		Assert.assertFalse(isElementEnabled(interestsDisCheckbox));
+		Assert.assertFalse(isElementEnabled(slider02));
+		Assert.assertFalse(isElementEnabled(buttonDisable));
 	}
-}
-  
+
+	@Test
+	public void TC_03_CheckElementIsSelected() {
+		checkToCheckbox(ageUnder18Radio);
+		checkToCheckbox(interestDevCheckbox);
+		Assert.assertTrue(isElementSelected(ageUnder18Radio));
+		Assert.assertTrue(isElementSelected(interestDevCheckbox));
+		unCheckToCheckbox(interestDevCheckbox);
+		Assert.assertFalse(isElementSelected(interestDevCheckbox));;
+	}
+
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
+	public boolean isElementDisplayed(By byVAlue) {
+		return driver.findElement(byVAlue).isDisplayed();
+	}
+
+	public boolean isElementEnabled(By byValue) {
+		if(driver.findElement(byValue).isEnabled()) {
+			System.out.println(driver.findElement(byValue).getTagName() + " --- Element is Enabled");
+			return true;
+		}else {
+			System.out.println("Element: [ "+ byValue +"] Element is Disabled");
+			return false;
+		}
+	}
+	
+	public boolean isElementSelected(By byValue) {
+		WebElement element = driver.findElement(byValue);
+		  if(element.isSelected()) {
+			  System.out.println("Element [ "+byValue+" is selected");
+			  return true;
+		  } else {
+			  System.out.println("Element [ "+byValue+" is de-selected");
+			  return false;
+		  }
+	}
+	
+	public void checkToCheckbox(By byValue) {
+		WebElement element = driver.findElement(byValue);
+		  if(!element.isSelected()) {
+			  element.click();
+		  } 
+	}
+
+	public void unCheckToCheckbox(By byValue) {
+		WebElement element = driver.findElement(byValue);
+		  if(element.isSelected()) {
+			  element.click();
+		  } 
+	}
 }
---
+++ src/selenium/Topic_04_TextBox_TextArea.java
@@ -0,0 +1,145 @@
+package selenium;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.browserlaunchers.locators.FirefoxLocator;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+
+public class Topic_04_TextBox_TextArea {
+	WebDriver driver;
+	String customerName, gender, dateOfBirth, address, city, state, pin, phone, email, password, customerId;
+	String editAddess, editCity, editState, editPin, editPhone, editEmail;
+
+	//Locate Element
+	By cutomerNameTextbox= By.xpath("//input[@name='name']");
+	By genderRadio = By.xpath("//input[@value='m']");
+	By dateOrBirthTextbox = By.xpath("//input[@id='dob']");
+	By addressTextArea = By.xpath("//textarea[@name='addr']");
+	By cityTextbox = By.xpath("//input[@name='city']");
+	By stateTextbox = By.xpath("//input[@name='state']");
+	By pinTextBox = By.xpath("//input[@name='pinno']");
+	By phoneTextBox = By.xpath("//input[@name=\"telephoneno\"]");
+	By emailTextBox = By.xpath("//input[@name=\"emailid\"]");
+	By passwordTextBox = By.xpath("//input[@name=\"password\"]");
+	By submitButton = By.xpath("//input[@name=\"sub\"]");
+
+	@BeforeTest
+	public void beforeTest() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window();
+		driver.get("http://demo.guru99.com/v4/");
+		driver.findElement(By.xpath("//input[@name=\"uid\"]")).sendKeys("mngr181358 ");
+		driver.findElement(By.xpath("//input[@name=\"password\"]")).sendKeys("berydUp");
+		driver.findElement(By.xpath("//input[@name=\"btnLogin\"]")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//marquee[text()=\"Welcome To Manager's Page of Guru99 Bank\"]")).isDisplayed());
+		//Init data test for new
+		customerName = "Automation Test";
+		gender = "male";
+		dateOfBirth = "1999-01-01";
+		address = "1234 Le Loi";
+		city = "Ho Chi Minh";
+		state = "Hoan Kiem";
+		pin = "600000";
+		phone = "012343546573";
+		email = "aumationtest" + ranDomEmail() +"@gmail.com";
+		password = "123123";
+		
+		//Init data test for Edit
+		editAddess = "2345 Hung Vuong";
+		editCity = "Da Nang";
+		editState = "Hai Chau";
+		editPin = "550000";
+		editPhone = "0987653211";
+		editEmail = "editautomate"+ranDomEmail()+"@gmail.com";
+	}
+
+	@Test
+	public void TC_01_CreateNewCustomer() {
+		driver.findElement(By.xpath("//a[text()='New Customer']")).click();
+		Assert.assertTrue(driver.findElement(By.xpath("//p[text()='Add New Customer']")).isDisplayed());
+		// Input data for new customer
+		driver.findElement(cutomerNameTextbox).sendKeys(customerName);
+		driver.findElement(genderRadio).click();
+		driver.findElement(dateOrBirthTextbox).sendKeys(dateOfBirth);
+		driver.findElement(addressTextArea).sendKeys(address);
+		driver.findElement(cityTextbox).sendKeys(city);
+		driver.findElement(stateTextbox).sendKeys(state);
+		driver.findElement(pinTextBox).sendKeys(pin);
+		driver.findElement(phoneTextBox).sendKeys(phone);
+		driver.findElement(emailTextBox).sendKeys(email);
+		driver.findElement(passwordTextBox).sendKeys(password);
+		driver.findElement(submitButton).click();
+		// Verify expected data = actual data 
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class=\"heading3\" and text()='Customer Registered Successfully!!!']")).isDisplayed());
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Customer Name']/following-sibling::td")).getText(), customerName);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Gender']/following-sibling::td")).getText(), gender);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Birthdate']/following-sibling::td")).getText(), dateOfBirth);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(), address);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(), city);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(), state);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(), pin);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(), phone);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(), email);
+		
+		customerId = driver.findElement(By.xpath("//td[text()='Customer ID']/following-sibling::td")).getText();
+		System.out.println("Customer ID at TC 01 = " + customerId);
+	}
+	
+	@Test
+	public void TC_02_EditCustomer() {
+		driver.findElement(By.xpath("//a[text()='Edit Customer']")).click();
+		driver.findElement(By.xpath("//input[@name='cusid']")).sendKeys(customerId);
+		driver.findElement(By.xpath("//input[@name='AccSubmit']")).click();
+		
+		//Input data for edit customer
+		driver.findElement(addressTextArea).clear();
+		driver.findElement(addressTextArea).sendKeys(editAddess);
+		driver.findElement(cityTextbox).clear();
+		driver.findElement(cityTextbox).sendKeys(editCity);
+		driver.findElement(stateTextbox).clear();
+		driver.findElement(stateTextbox).sendKeys(editState);
+		driver.findElement(pinTextBox).clear();
+		driver.findElement(pinTextBox).sendKeys(editPin);
+		driver.findElement(phoneTextBox).clear();
+		driver.findElement(phoneTextBox).sendKeys(editPhone);
+		driver.findElement(emailTextBox).clear();
+		driver.findElement(emailTextBox).sendKeys(editEmail);
+		driver.findElement(By.xpath("//input[@value='Submit']")).click();
+		
+		// Verify expected data = actual data 
+		Assert.assertTrue(driver.findElement(By.xpath("//p[@class='heading3' and text()='Customer details updated Successfully!!!']")).isDisplayed());
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(), editAddess);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(), editCity);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(), editState);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(), editPin);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(), editPhone);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(), editEmail);
+		
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+
+	public int ranDomEmail() {
+		// Create random to email address
+		Random email = new Random();
+		//Obtain a number between [0-1000]
+		int n = email.nextInt(1000);
+		// Add 1 to the result to get a number from the required range
+		// (i.e., [1 - 100]).
+		n += 1;
+		return n;
+	}
+}
